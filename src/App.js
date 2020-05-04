fa779b9386dc0b2ad3ba19366a4a896f7f53040b
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-05 00:58:08 +0530

    v3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Table from './Table'
+class App extends Component {
+	render() {
+		return(
+			<div className = 'container'>
+				<Table />
+			</div>
+		)
+	}
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-class App extends Component {
-	render() {
-		return(
-			<div className = 'App'>
-				<h1>Hello, React!</h1>
-			</div>
-		)
-	}
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'))
