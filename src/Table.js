de90936f65dfec28d3a87f2453150269222dc992
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-05 01:04:57 +0530

    v4

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-class Table extends Component {
+/*class Table extends Component {
 	render(){
 		return(
 			<table>
@@ -35,6 +35,54 @@ class Table extends Component {
 			</table>
 		)
 	}
+}*/
+
+const TableHeader = () => {
+	return (
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Job</th>
+			</tr>
+		</thead>
+	)
 }
 
+const TableBody = () => {
+	return(
+		<tbody>
+			<tr>
+				<td>
+				Charlie</td>
+				<td>Janitor</td>
+			</tr>
+			<tr>
+				<td>
+				Mac</td>
+				<td>Bouncer</td>
+			</tr>
+			<tr>
+				<td>
+				Dee</td>
+				<td>Aspiring actress</td>
+			</tr>
+			<tr>
+				<td>
+				Dennis</td>
+				<td>Bartender</td>
+			</tr>
+		</tbody>
+	)
+}
+
+class Table extends Component {
+	render(){
+		return(
+			<table>
+				<TableHeader />
+				<TableBody />
+			</table>
+		)
+	}
+}
 export default Table
