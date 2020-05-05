cbcb5699452c261cb03ebda43de5988d206b4e31
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-05 14:17:55 +0530

    v5

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React, {Component} from 'react';
 import Table from './Table'
 class App extends Component {
 	render() {
+
+		const characters = [
+			{
+				name: 'Charlie',
+				job: 'Janitor'
+			},
+			{
+				name : 'Mac',
+				job:'Bouncer'
+			},
+			{
+				name : 'Dee',
+				job : 'Aspiring actress'
+			},
+			{
+				name :'Dennis',
+				job: 'Bartender'
+			}
+		]
+
 		return(
 			<div className = 'container'>
-				<Table />
+				<Table characterData = {characters} />
 			</div>
 		)
 	}
--- src/Table.js
+++ src/Table.js
@@ -49,40 +49,18 @@ const TableHeader = () => {
 }
 
 const TableBody = () => {
-	return(
-		<tbody>
-			<tr>
-				<td>
-				Charlie</td>
-				<td>Janitor</td>
-			</tr>
-			<tr>
-				<td>
-				Mac</td>
-				<td>Bouncer</td>
-			</tr>
-			<tr>
-				<td>
-				Dee</td>
-				<td>Aspiring actress</td>
-			</tr>
-			<tr>
-				<td>
-				Dennis</td>
-				<td>Bartender</td>
-			</tr>
-		</tbody>
-	)
+	return <tbody />
 }
 
 class Table extends Component {
 	render(){
+		const {characterData} = this.props;
 		return(
 			<table>
 				<TableHeader />
-				<TableBody />
+				<TableBody characterData = {characterData} />
 			</table>
 		)
 	}
 }
-export default Table
+export default Table 
