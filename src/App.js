6c6a0b23bc1dd3f9f87ba558c2358dd0136315d9
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-05 15:36:55 +0530

    v6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
 import Table from './Table'
 class App extends Component {
-	render() {
-
-		const characters = [
+	state = {
+		characters : [
 			{
 				name: 'Charlie',
 				job: 'Janitor'
@@ -21,10 +20,41 @@ class App extends Component {
 				job: 'Bartender'
 			}
 		]
+	}
 
+	removeCharacter = index => {
+		const {characters} = this.state
+		this.setState({
+			characters: characters.filter((characters,i) => {
+				return i !== index
+			}),
+		})
+	}
+
+	render() {
+
+		/*const characters = [
+			{
+				name: 'Charlie',
+				job: 'Janitor'
+			},
+			{
+				name : 'Mac',
+				job:'Bouncer'
+			},
+			{
+				name : 'Dee',
+				job : 'Aspiring actress'
+			},
+			{
+				name :'Dennis',
+				job: 'Bartender'
+			}
+		]*/
+		const {characters} = this.state
 		return(
 			<div className = 'container'>
-				<Table characterData = {characters} />
+				<Table characterData = {characters} removeCharacter = {this.removeCharacter} />
 			</div>
 		)
 	}
--- src/Table.js
+++ src/Table.js
@@ -48,19 +48,30 @@ const TableHeader = () => {
 	)
 }
 
-const TableBody = () => {
-	return <tbody />
+const TableBody = (props) => {
+	const rows = props.characterData.map((row,index) => {
+		return(
+			<tr key = {index}>
+				<td>{row.name}</td>
+				<td>{row.job}</td>
+				<td>
+					<button onClick = {() => props.removeCharacter(index) } >Delete</button>
+				</td>
+			</tr>
+		)
+	})
+
+	return <tbody>{rows}</tbody>
 }
 
-class Table extends Component {
-	render(){
-		const {characterData} = this.props;
+const Table = props => {
+	const {characterData, removeCharacter } = props
+		/*const {characterData} = this.props;*/
 		return(
 			<table>
 				<TableHeader />
-				<TableBody characterData = {characterData} />
+				<TableBody characterData = {characterData} removeCharacter = {removeCharacter} />
 			</table>
 		)
-	}
 }
 export default Table 
