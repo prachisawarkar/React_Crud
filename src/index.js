e6a1a0469021c1c2ae0654a0ddced70f342766d8
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-06 22:14:17 +0530

    v8

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Api';
 
 ReactDOM.render(<App />, document.getElementById('root'))
