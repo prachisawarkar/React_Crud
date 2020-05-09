4699ac7e8b31787947c9886e051cf30264ecbe4d
PrachiSawarkar <prachisawarkar3@gmail.com>
2020-05-09 14:27:01 +0530

    Updates

---
+++ precache-manifest.cde08127fb79448e995d80b474a3fc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9fe07ffd99db40971b1e2a2a6b5e70",
+    "url": "/prachisawarkar/React/index.html"
+  },
+  {
+    "revision": "14a4bc1a5a1058683f50",
+    "url": "/prachisawarkar/React/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "7777b9f18b5549353e1b",
+    "url": "/prachisawarkar/React/static/js/2.5e478115.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prachisawarkar/React/static/js/2.5e478115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a4bc1a5a1058683f50",
+    "url": "/prachisawarkar/React/static/js/main.27249854.chunk.js"
+  },
+  {
+    "revision": "884524c8ced10bc1a156",
+    "url": "/prachisawarkar/React/static/js/runtime-main.72d1307d.js"
+  }
+]);
